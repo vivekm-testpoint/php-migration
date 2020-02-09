0dffadc28e09dd7136cbde431064727363201651
vivekm-testpoint <vivek.m@testpoint.com.au>
2020-02-09 21:59:25 +0530

    Create env.migration.php

---
+++ env.migration.php
@@ -0,0 +1,8 @@
+<?php
+define('PHP_MIGRATION_DB_HOST', '');
+define('PHP_MIGRATION_DB_USERNAME', '');
+define('PHP_MIGRATION_DB_PASSWORD', '');
+define('PHP_MIGRATION_DB_NAME', '');
+
+define('PHP_MIGRATION_TABLE_NAME', 'migration_versions');
+define('PHP_MIGRATION_PATH', 'migrations/');
